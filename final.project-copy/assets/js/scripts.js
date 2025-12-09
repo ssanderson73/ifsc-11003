934aa441282afaedcfa1001996df7e394d8f4dfe
ssanderson73 <ssanderson@ualr.edu>
2025-12-09 07:26:31 +0000

    fixed img map

--- final.project-copy/assets/js/scripts.js
+++ final.project-copy/assets/js/scripts.js
@@ -14,10 +14,25 @@ for (i = 0; i < acc.length; i++) {
 }
 
 
-function highlightItem(elemment) {
-    elemment.classList.add("highlight");
-}
 
-function removeHighlight(elemment) {
-    elemment.classList.remove("highlight")
-}
+
+
+
+
+//select all map areas
+const areas =document.querySelectorAll("map[name='workmap'] area");
+
+//add hover listeners
+areas.forEach(area => {
+  const keyValue = area.dataset.key;
+
+  area.addEventListener("mouseenter", () => {
+    const li = document.querySelector(`.key li[data-key="${keyValue}"]`);
+    if (li) li.classList.add("highlight");
+  });
+
+  area.addEventListener("mouseleave", () => {
+    const li = document.querySelector(`.key li[data-key="${keyValue}"]`);
+    if (li) li.classList.remove("highlight");
+  });
+});
